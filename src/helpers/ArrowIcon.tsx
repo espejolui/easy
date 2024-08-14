fb92a1088d8a99d273ac08f5d214c2f9b3b89ccd
lucho <herrera.espejo.luis@gmail.com>
2024-08-14 16:29:56 -0500

    Corrección de propiedades stroke con guion al medio y dejandolas con cammel case

--- src/helpers/ArrowIcon.tsx
+++ src/helpers/ArrowIcon.tsx
@@ -7,9 +7,9 @@ export const ArrowIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="1.3"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M5 12l14 0" />
