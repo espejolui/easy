31e286f17523bee5b6c9c2a837068f49892a6777
lucho <herrera.espejo.luis@gmail.com>
2024-08-18 11:15:11 -0500

    actualización de rutas

--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Feed } from "../components/pages/feed/Feed";
 import { Calendar } from "../components/pages/calendar/Calendar";
 import { Error } from "../components/pages/error/Error";
+import { Ajiaco } from "../components/pages/recipes/Ajiaco";
 
 export const RoutesMain = () => {
   return (
@@ -10,6 +11,7 @@ export const RoutesMain = () => {
         <Route path="/" element={<Feed />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/calendario" element={<Calendar />} />
+        <Route path="/ajiaco" element={<Ajiaco />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </>
